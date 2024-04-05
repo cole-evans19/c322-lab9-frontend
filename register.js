24b21862cfca0575be46241cd94948fa7967e3c3
Cole Evans <ce9@iu.edu>
2024-04-04 23:13:56 -0400

    final

--- register.js
+++ register.js
@@ -8,7 +8,8 @@ async function signup() {
     let request = {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            'Authorization': `Bearer ${configuration.token()}`
         },
         body: JSON.stringify(customer)
       };
@@ -17,7 +18,8 @@ async function signup() {
         if(response.status == 200) {  
             alert("The registration was successful!")
             location.href = "login.html";
-
+        } else if(response.status == 401){
+          logout();
         } else {
             console.log(`response status:${response.status}`);            
             alert("Something went wrong!");
